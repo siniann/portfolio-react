8c8aee4718ac0e986a803450488a9eaf9d16d333
Sini Ann Padinjarekuttu <siniann.777@gmail.com>
2020-11-17 18:04:58 +0100

    Add scroll component

--- src/components/my-corousal/my-corousal.component.jsx
+++ src/components/my-corousal/my-corousal.component.jsx
@@ -5,7 +5,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import Slide1 from '../../assets/img/carousal/slide1.webp';
 import Slide2 from '../../assets/img/carousal/slide2.webp';
 import Slide3 from '../../assets/img/carousal/slide3.webp';
-
+import ScrollDown from '../scroll-component/scroll-component.component';
 import './my-corousal.style.css';
 
 const MyCorousal = () => {
@@ -34,6 +34,7 @@ const MyCorousal = () => {
                     />
                 </Carousel.Item>
             </Carousel>
+            <ScrollDown />
         </div>
     )
 }
